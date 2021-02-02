cfea043b87498cf7ee77a177579bfc93b2528bac
Baki <ibalaz66@gmail.com>
2021-02-02 21:18:46 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {Button, Card, CardContent, Grid, makeStyles, Paper, TextField} from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        flexGrow: 1,
+    },
+    paper: {
+        padding: theme.spacing(2),
+        textAlign: 'center',
+        color: theme.palette.text.secondary,
+    },
+}));
+
+const App = () => {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <Grid container spacing={2} alignItems="flex-end">
+                <Grid item xs direction="row" justify="left" alignItems="center" >
+                    <Paper>Logo</Paper>
+                </Grid>
+                <Grid item xs={9} direction="row" justify="left" alignItems="center" >
+                    <Paper>Default</Paper>
+                </Grid>
+            </Grid>
+            <Grid container justify="center">
+                <Card>
+                    <CardContent>
+                        <form noValidate autoComplete="off">
+                            <TextField required id="tokenSN" label="Serijski broj tokena" variant="outlined" />
+                            <TextField required id="otp" label="APPLI 1" variant="outlined" />
+                        </form>
+                    </CardContent>
+                </Card>
+            </Grid>
+            <Grid container spacing={3} justify="center" alignItems="flex-end">
+                <Button>Kontakt centar</Button>
+                <Button>Pomoć</Button>
+                <Button>Nešto</Button>
+            </Grid>
+        </div>
+
+    )
 }
 
 export default App;
